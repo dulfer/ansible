9757f2f042f7694f970885508a8667934fc7b38b
dulfer <30404904+dulfer@users.noreply.github.com>
2025-02-17 19:56:47 +0100

    Adding unzip to req apt packages

--- setup/vaultwarden-cli.sh
+++ setup/vaultwarden-cli.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+echo "This script will install the Bitwarden CLI on your system."
+echo "Sudo is required, expect a password prompt."
+echo "---"
 
 # ensuring curl is installed
-sudo apt install curl -y
+sudo apt install curl unzip -y
 
 # download latest bitwarden cli
 curl -L "https://vault.bitwarden.com/download/?app=cli&platform=linux" > /tmp/bw.zip
 
 # unzip and move to /usr/local/bin
 unzip /tmp/bw.zip -d /tmp
-sudo mv /tmp/bw /usr/local/bin
+sudo mv /tmp/bw /usr/local/bin/
 
 # make executable
 sudo chmod +x /usr/local/bin/bw
